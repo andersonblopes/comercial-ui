35e7b0d50d7477ad7146e4da337c3707c2c95807
anderson.lopes@altran.com <anderson.lopes@altran.com>
2019-10-01 00:45:22 +0100

    Prepara listagem.

--- src/app/painel-negociacao/painel-negociacao.component.ts
+++ src/app/painel-negociacao/painel-negociacao.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelNegociacaoComponent implements OnInit {
 
-  nome = 'Anderson';
+  oportunidades = [
+    {descricao: 'Projeto de desenvolvimento', nomeProspecto: 'Anderson', valor: 55000},
+    {descricao: 'Projeto de Suporte', nomeProspecto: 'Pedro', valor: 35000},
+    {descricao: 'Projeto de Vendas', nomeProspecto: 'Jeane', valor: 4560000},
+    {descricao: 'Projeto de DEVOPS', nomeProspecto: 'Elaine', valor: 255},
+    {descricao: 'Projeto de Implantação', nomeProspecto: 'Anderson', valor: 67500},
+    {descricao: 'Projeto de Testes', nomeProspecto: 'Maria da Silva', valor: 200}  
+];
 
   constructor() { }
 
